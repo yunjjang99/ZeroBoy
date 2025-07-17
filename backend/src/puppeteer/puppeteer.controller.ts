a06aa5af1cfc7924f4dea2210be044d88ce5b385
shin yun soo <shin4895@gmail.com>
2025-07-17 14:57:03 +0900

    fix : API 리턴 데이터형식에 맞게 변경
    success failure 형태로 반환하도록함

--- backend/src/puppeteer/puppeteer.controller.ts
+++ backend/src/puppeteer/puppeteer.controller.ts
@@ -1,19 +1,29 @@
 // src/puppeteer-test/puppeteer.controller.ts
 import { Controller, Post, Body } from "@nestjs/common";
 import { PuppeteerService } from "./puppeteer.service";
+import { failure, success } from "@/utils/functionalUtil";
 
-@Controller("puppeteer-test")
+@Controller("puppeteer")
 export class PuppeteerController {
   constructor(private readonly puppeteerService: PuppeteerService) {}
 
   @Post("launch")
   async launchBrowser(@Body("url") url: string) {
     const browser = await this.puppeteerService.createBrowser();
-    const page = await browser.newPage();
-    await page.goto(url, { waitUntil: "networkidle2" });
+
+    const pages = await browser.pages();
+    const page = pages[0]; // 첫 번째 탭
+
+    // await page.goto(url, { waitUntil: "networkidle2", timeout: 10000 });
     const title = await page.title();
-    console.log(title, "타이틀");
     // await page.close();
-    return { status: "ok", title };
+    return success({ title });
+  }
+  catch(err) {
+    console.error("Puppeteer Error:", err);
+    return failure({
+      message: "브라우저 실행 중 오류가 발생했습니다.",
+      status: 500,
+    });
   }
 }
